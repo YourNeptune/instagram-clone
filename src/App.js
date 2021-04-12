b0bab84cf659a568d3f656ee14da44068a6d960f
Lixuan Luo <lixuanluo@hotmail.com>
2021-04-11 21:39:43 -0400

    firebase auth (sign up)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,48 @@ import "./css/App.css";
 import Post from "./components/Post";
 import Signup from "./components/Signup";
 import { useEffect, useState } from "react";
-import { db } from "./firebase";
+import { db, auth } from "./firebase";
 import { Button, Modal } from "@material-ui/core";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [user, setUser] = useState(null);
+
+  const signup = (e) => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((err) => alert(err.message));
+  };
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has logged in
+        console.log("✅", authUser);
+        setUser(authUser);
+      } else {
+        // user has logged out
+        setUser(null);
+      }
+    });
+
+    console.log("User:", user);
+
+    return () => {
+      // perform some cleanup actions
+    };
+  }, [user]);
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
@@ -18,7 +54,17 @@ function App() {
   return (
     <div className="App">
       <Modal open={open} onClose={() => setOpen(false)}>
-        <Signup />
+        <>
+          <Signup
+            email={email}
+            username={username}
+            password={password}
+            onSignup={signup}
+            onSetEmail={(e) => setEmail(e.target.value)}
+            onSetUsername={(e) => setUsername(e.target.value)}
+            onSetPassword={(e) => setPassword(e.target.value)}
+          />
+        </>
       </Modal>
       <div className="App__header">
         <img
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
-import { Button, Input, makeStyles, OutlinedInput } from "@material-ui/core";
-import React, { useState } from "react";
+import { Button, makeStyles, OutlinedInput } from "@material-ui/core";
+import { forwardRef, useState } from "react";
 import "../css/Signup.css";
 
 function getModalStyle() {
@@ -24,59 +24,77 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Signup = () => {
-  const classes = useStyles();
-  const [modalStyle] = React.useState(getModalStyle);
+const Signup = forwardRef(
+  (
+    {
+      email,
+      username,
+      password,
+      onSignup,
+      onSetEmail,
+      onSetUsername,
+      onSetPassword,
+    },
+    ref
+  ) => {
+    const classes = useStyles();
+    const [modalStyle] = useState(getModalStyle);
 
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  return (
-    <div style={modalStyle} className={`${classes.paper} paper__items`}>
-      <img
-        className="paper__logo"
-        alt="instagram-logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
-      />
-
-      <div className="paper__body">
-        <h4 className="paper__text">
-          Sign up to see photos and videos from your friends.
-        </h4>
-        <OutlinedInput
-          className="paper__item"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+    return (
+      <div
+        ref={ref}
+        style={modalStyle}
+        className={`${classes.paper} paper__items`}
+      >
+        <img
+          className="paper__logo"
+          alt="instagram-logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
         />
-        <OutlinedInput
-          className="paper__item"
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <OutlinedInput
-          className="paper__item"
-          type="text"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button className="paper__btn" type="submit" variant="contained">
-          Sign up
-        </Button>
 
-        <p className="paper__text paper__textp">
-          By signing up, you agree to our <strong>Terms</strong> ,
-          <strong>Data Policy</strong>and <strong>Cookies Policy</strong>.
-          (dummy text)
-        </p>
+        <div className="paper__body">
+          <h4 className="paper__text">
+            Sign up to see photos and videos from your friends.
+          </h4>
+          <OutlinedInput
+            className="paper__item"
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={onSetEmail}
+          />
+          <OutlinedInput
+            className="paper__item"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={onSetUsername}
+          />
+          <OutlinedInput
+            className="paper__item"
+            type="text"
+            placeholder="Password"
+            value={password}
+            onChange={onSetPassword}
+          />
+          <Button
+            className="paper__btn"
+            type="submit"
+            variant="contained"
+            onClick={onSignup}
+          >
+            Sign up
+          </Button>
+
+          <p className="paper__text paper__textp">
+            By signing up, you agree to our <strong>Terms</strong> ,
+            <strong>Data Policy</strong>and <strong>Cookies Policy</strong>.
+            (dummy text)
+          </p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Signup;
